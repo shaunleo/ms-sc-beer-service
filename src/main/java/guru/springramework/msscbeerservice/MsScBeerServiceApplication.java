8561ee91295dc1f441cf3f3a8600292c74c7cfb6
Shaun Leong <shaunleo@gmail.com>
2021-08-19 12:27:16 +0800

    Initial Project Add

---
+++ src/main/java/guru/springramework/msscbeerservice/MsScBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package guru.springramework.msscbeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsScBeerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsScBeerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springramework/msscbeerservice/MsScBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springramework.msscbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsScBeerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
